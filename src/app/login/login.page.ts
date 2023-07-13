dbd7a6e76e2eb42f214a7fd1d00f03f3a6aaa996
Carl Xyreuss Adaya <cxyreussadaya@gmail.com>
2023-07-13 20:53:45 +0800

    src file

---
+++ src/app/login/login.page.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { NavController } from '@ionic/angular';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss'],
+})
+export class LoginPage implements OnInit {
+
+constructor(private navCtrl: NavController) {}
+
+  ngOnInit() {
+  }
+  redirectToHome() {
+    this.navCtrl.navigateRoot('/home');
+  }
+}
