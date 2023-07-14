3447e5665dd2a14da12b603034afb7eab78aa530
Carl Xyreuss Adaya <cxyreussadaya@gmail.com>
2023-07-14 20:37:41 +0800

    src2

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { NavController } from '@ionic/angular';
-import { Router } from '@angular/router';
+import { NavController, AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-login',
@@ -8,12 +7,29 @@ import { Router } from '@angular/router';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
+  username!: string;
+  password!: string;
 
-constructor(private navCtrl: NavController) {}
+constructor(
+  private navCtrl: NavController, 
+  private alertController: AlertController
+  ) {}
 
   ngOnInit() {
   }
   redirectToHome() {
     this.navCtrl.navigateRoot('/home');
+  }
+  async login() {
+    if (this.username === 'admin' && this.password === 'admin') {
+      this.navCtrl.navigateForward('/home');
+    } else {
+      const alert = await this.alertController.create({
+        header: 'Login Failed',
+        message: 'Invalid credentials',
+        buttons: ['try again']
+      });
+      await alert.present();
+    }
   }
 }
